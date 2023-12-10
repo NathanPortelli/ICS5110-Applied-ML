3b48f2e202985a98890c3c788844f744a89e7c59
Nathan Camilleri <nathancamilleri6@gmail.com>
2023-12-10 16:41:04 +0100

    Added further evaluation metrics + plots

--- Utils.py
+++ Utils.py
@@ -4,3 +4,10 @@ import numpy as np
 class Utils:
     def root_mean_squared_error(test_data, predictions):
         return np.sqrt(mean_squared_error(test_data, predictions))
+
+    def quantile_loss(y_true, y_pred, alpha):
+        y_true = np.array(y_true)
+        y_pred = np.array(y_pred)
+        epsilon = 1e-10
+        e = y_true - y_pred
+        return np.sum(np.where(e >= 0, alpha * e, (alpha - 1) * e))
